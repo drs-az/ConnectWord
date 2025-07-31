bae815ab05ef45ee2ddcc398b201f65ffea42df9
drs-az <204786705+drs-az@users.noreply.github.com>
2025-07-31 09:20:57 -0700

    Fix service worker and init game after DOM loads

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'connectword-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/main.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
